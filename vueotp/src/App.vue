522e92de0e3e78b95a326b56252baa38b663727b
jojooyoo <115844041+jojooyoo@users.noreply.github.com>
2022-10-23 18:50:51 +0300

    otp

---
+++ vueotp/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <otp 
+    :digit-count="4"
+    @update:otp="otpValue = $event"
+  ></otp>
+
+  <p>The current OTP value is: {{ otpValue }} </p>
+</div>
+</template>
+
+<script setup>
+import otp from "./components/OTP.vue";
+
+import { ref } from "vue";
+
+otpValue = ref('')
+</script>
+
+<style>
+
+</style>
